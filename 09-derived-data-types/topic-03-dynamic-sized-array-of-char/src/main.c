f427f0cb7210e8b7f45b87a038637f6cab8cd7eb
dezlymacauley <dezlymacauley@proton.me>
2026-02-21 15:33:56 +0200

    Added guide on dynamic-sized array of char

---
+++ 09-derived-data-types/topic-03-dynamic-sized-array-of-char/src/main.c
@@ -0,0 +1,27 @@
+/*
+    ABOUT: dynamic array of char
+
+*/
+
+#include <stdio.h>
+
+int main() {
+
+    //_________________________________________________________________________
+
+    // SECTION: How to declare the array
+
+    char* currency_code = "eur";
+
+    // TIP: When using double quotes the null terminator `\0` 
+    // is automatically added.
+
+    //_________________________________________________________________________
+
+    // SECTION: How to print the contents of the array
+
+    printf("%s\n", currency_code);
+    // e, u, r
+
+    //_________________________________________________________________________
+}

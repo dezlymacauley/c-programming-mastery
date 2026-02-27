57d5e9bf82551a60ea3290029b19be089ef20746
dezlymacauley <dezlymacauley@proton.me>
2026-02-27 12:40:16 +0200

    Changed error handling to stderr

--- 14-reading-from-files/topic-02-running-the-main-function-with-arguments/src/main.c
+++ 14-reading-from-files/topic-02-running-the-main-function-with-arguments/src/main.c
@@ -7,7 +7,7 @@
 // This is required to use:
 // `printf()`
 
-// `argc` which stands for argument count, 
+// `argc` which stands for argument count,
 // is the number of command line arguments passed to the `main` function
 
 //  `argv` which stands for argument vector,
@@ -18,19 +18,42 @@ int main(int argc, char* argv[]) {
     // The first arguement at index 0 of argv is this:
     // argv[0] is: ./bin/main
 
+    //_________________________________________________________________________
 
-    // TIP: To supply additional arguments (arv[1], argv[2] and so on),
-    // do this, use `ARGS=` for EACH additional argument.
+    // SECTION: How to supply additional arguments (arv[1],
+    // argv[2] and so on), from the Makefile
 
     // make run ARGS="argument_one, argument_two"
     // E.g.
-    // make run ARGS="kamen" ARGS="rider"
+    // make run ARGS="kamen rider"
+
+    // See the Makefile in this project
+
+    //_________________________________________________________________________
+
+    // SECTION: How to supply additional arguments (arv[1],
+    // argv[2] and so on),
+    // from the command line (Using the binary executable)
+
+    // Do this:
+    // ./bin/main kamen rider
+
+    //_________________________________________________________________________
 
     // Its three arguments in total
     // argv[0] + two arguments from the user
     if (argc != 3) {
-        printf("You need to supply two arguments\n");
+        fprintf(stderr, "You need to supply two arguments\n");
+        fprintf(stderr, "Usage:\n");
+        fprintf(stderr, "program_name argument_one argument_two\n");
+        return 1;
     }
+
+    printf("argv[1] is: %s\n", argv[1]);
+    // argv[1] is: kamen
+
+    printf("argv[2] is: %s\n", argv[2]);
+    // argv[2] is: rider
 
     return 0;
 }

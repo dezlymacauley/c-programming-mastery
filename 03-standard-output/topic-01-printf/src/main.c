fca4f339d1efdcd10f1d12e26ef088761e677020
dezlymacauley <dezlymacauley@proton.me>
2026-02-27 16:18:47 +0200

    Done with C for today

---
+++ 03-standard-output/topic-01-printf/src/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+
+    // printf() is used to print information to the standard ouput stream.
+    // which is the command line by default.
+
+    // The `\n` character means new line.
+    // This tells the compiler that after displaying "Hello world" 
+    // in the terminal, it should move the cursor down to the next line,
+    // and also move the cursor to the start of that line so that the next
+    // time you use `printf()` to print something, 
+    // that message will start on a new line.
+    printf("Message 1\n");
+    printf("Message 2\n");
+    printf("Message 3\n");
+
+}
